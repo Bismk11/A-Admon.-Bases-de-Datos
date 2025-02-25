186127bd2e8c1b1c08ae8f1038bbc029519891df
Bismk11 <s22120167@alumnos.itsur.edu.mx>
2025-02-24 21:47:59 -0600

    Add files via upload

---
+++ Unidad 2 Práctica 2.2 SQL SERVER - WINDOWS SERVER/Libros.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Libros.aspx.cs" Inherits="BibliotecaWeb.Libros" %>
+
+<!DOCTYPE html>
+
+<html lang="es">
+<head>
+<meta charset="utf-8"/>
+    <title>Biblioteca Digital</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <p>
+            ISBN:<asp:TextBox ID="txtISBN" runat="server"></asp:TextBox>
+        </p>
+        Título:<asp:TextBox ID="txtTitulo" runat="server"></asp:TextBox>
+        <p>
+            Número de Edición:<asp:TextBox ID="txtNoEdicion" runat="server"></asp:TextBox>
+        </p>
+        Año de Publicación:<asp:TextBox ID="txtPublicacion" runat="server"></asp:TextBox>
+        <p>
+            Nombre de los Autores:<asp:TextBox ID="txtAutores" runat="server"></asp:TextBox>
+        </p>
+        País de Publicación:<asp:TextBox ID="txtPais" runat="server"></asp:TextBox>
+        <p>
+            Sinopsis:<textarea id="txtaSinopsis" cols="20" name="S1" rows="2" runat="server"></textarea>
+        </p>
+        Carrera:<asp:TextBox ID="txtCarrera" runat="server"></asp:TextBox>
+        <p>
+            Materia:<asp:TextBox ID="txtMateria" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="btnRegistrar" runat="server" Text="Registrar" OnClick="btnRegistrar_Click" />
+        <br />
+        <br />
+        Lista de Libros<br />
+        <asp:GridView ID="gvLibros" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Unidad 2 Práctica 2.2 SQL SERVER - WINDOWS SERVER/Libros.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BibliotecaWeb
+{
+    public partial class Libros : System.Web.UI.Page
+    {
+        //Método para cargar los libros en el GridView
+        private void BindGrid()
+        {
+            string connString = System.Configuration.ConfigurationManager.ConnectionStrings["ConectarDB"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string query = "SELECT * FROM Libros";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                System.Data.DataTable dt = new System.Data.DataTable();
+                da.Fill(dt);
+                gvLibros.DataSource = dt;
+                gvLibros.DataBind();
+            }
+        }
+
+        //Cargar datos cuando la página se carga
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGrid();
+            }
+        }
+
+        protected void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            string connString = System.Configuration.ConfigurationManager.ConnectionStrings["ConectarDB"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string query = "INSERT INTO Libros (ISBN, Titulo, NumeroEdicion, AnnoPublicacion, NombreAutores, PaisPublicacion, Sinopsis, Carrera, Materia) " +
+                               "VALUES (@ISBN, @Titulo, @NumeroEdicion, @AnnoPublicacion, @NombreAutores, @PaisPublicacion, @Sinopsis, @Carrera, @Materia)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@ISBN", txtISBN.Text);
+                cmd.Parameters.AddWithValue("@Titulo", txtTitulo.Text);
+                cmd.Parameters.AddWithValue("@NumeroEdicion", int.Parse(txtNoEdicion.Text));
+                cmd.Parameters.AddWithValue("@AnnoPublicacion", int.Parse(txtPublicacion.Text));
+                cmd.Parameters.AddWithValue("@NombreAutores", txtAutores.Text);
+                cmd.Parameters.AddWithValue("@PaisPublicacion", txtPais.Text);
+                cmd.Parameters.AddWithValue("@Sinopsis", txtaSinopsis.Value);
+                cmd.Parameters.AddWithValue("@Carrera", txtCarrera.Text);
+                cmd.Parameters.AddWithValue("@Materia", txtMateria.Text);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                BindGrid(); //Actualiza el GridView después de agregar el libro
+            }
+        }
+    }
+}
